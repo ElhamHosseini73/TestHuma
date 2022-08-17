a13fd93d1f400b51b886ed7ddd15002000ac3948
Elham Hosseini <elhamhosseini.elh@gmail.com>
2022-08-17 14:30:41 +0430

    change in main  and name until createct  conflict

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
-print(" Add code in main in Master1")
-print(" Add code in main in Master2")
-print(" Add code in main in Master3")
-print(" print printName Merge whit Master")
-print(" Add code in main in printName1")
-print(" Add code in main in printName2")
-print(" Add code in main in printName3")
+print(" Add code in main in Master1 merge whit github")
+print(" Add code in main in Master2 merge whit github")
+print(" Add code in main in Master3 merge whit github")
+print(" print github Merge whit Master")
+print(" Add code in main in printName1 merge whit github")
+print(" Add code in main in printName2 merge whit github")
+print(" Add code in main in printName3 merge whit github")
 
